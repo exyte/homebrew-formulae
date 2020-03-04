8b4744cd93e75683797b7133259009c3d344175f
shipinev <dmitry.shipinev+github@gmail.com>
2020-03-04 16:15:59 +0700

    Initial commit

---
+++ Formula/swift-format.rb
@@ -0,0 +1,17 @@
+class SwiftFormat < Formula
+    desc "`swift-format` provides the formatting technology for SourceKit-LSP and the building blocks for doing code formatting transformations."
+    homepage "https://github.com/apple/swift-format"
+    url "https://github.com/apple/swift-format.git", :branch => "swift-5.1-branch"
+    version "0.0.1"
+
+    depends_on :xcode => ["11.0", :build]
+
+    def install
+        system "swift build -c release --disable-sandbox --build-path '.build'"
+        bin.install ".build/release/swift-format"
+    end
+
+    test do
+        system "#{bin}/swift-format --version"
+    end
+end
